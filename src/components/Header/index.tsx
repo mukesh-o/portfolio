8abd6414568f57f54b0dffee4ab0538298d465d8
Mukesh Sharma <mukesh.m1127@gmail.com>
2021-12-08 15:00:42 +0530

    Updated header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,25 +3,30 @@ import React from "react";
 import name from "../../images/name.png";
 
 const Header: React.FC = () => {
-  const headerRef = React.useRef<HTMLDivElement | null>(null)
-  const rootEl = document.getElementById("root")
-
-  React.useEffect(() => {
-    console.log(headerRef)
-  }, [headerRef])
-
   return (
     <section className="py-4 px-10 flex justify-between fixed w-screen">
       <div>
         <img src={name} alt="name" />
       </div>
       <div className="flex space-x-10 text-gray-400 pr-20">
-        <span className="self-center cursor-pointer hover:text-white">Home</span>
-        <span className="self-center cursor-pointer hover:text-white">Services</span>
-        <span className="self-center cursor-pointer hover:text-white">Work</span>
-        <span className="self-center cursor-pointer hover:text-white">Skills</span>
-        <span className="self-center cursor-pointer hover:text-white">Blogs</span>
-        <span className="self-center cursor-pointer hover:text-white">Contact</span>
+        <span className="self-center cursor-pointer hover:text-white">
+          Home
+        </span>
+        <span className="self-center cursor-pointer hover:text-white">
+          Services
+        </span>
+        <span className="self-center cursor-pointer hover:text-white">
+          Work
+        </span>
+        <span className="self-center cursor-pointer hover:text-white">
+          Skills
+        </span>
+        <span className="self-center cursor-pointer hover:text-white">
+          Blogs
+        </span>
+        <span className="self-center cursor-pointer hover:text-white">
+          Contact
+        </span>
       </div>
     </section>
   );
